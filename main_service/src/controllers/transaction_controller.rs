d4cb60b6198974ad21d5fb373457c7eadcc9f8b7
Zachary Fanelle <zachary.fanelle@gmail.com>
2021-03-30 11:36:57 -0400

    feat(tickdap): add get transactions to controllers

--- main_service/src/controllers/transaction_controller.rs
+++ main_service/src/controllers/transaction_controller.rs
@@ -4,11 +4,10 @@ use super::services::transaction_service::get_all_transactions as service_get_al
 use super::services::transaction_service::get_transaction as service_get_transaction;
 use crate::config::AppConfig;
 use actix_web::{get, post, web, Error, HttpResponse};
-use log::debug;
 use serde_json::to_string;
 
 #[post("/transaction")]
-pub async fn create_transacton(
+pub async fn create_transaction(
     transaction: web::Json<TransactionDTO>,
     app_config: web::Data<AppConfig>,
 ) -> Result<HttpResponse, Error> {
@@ -19,4 +18,25 @@ pub async fn create_transacton(
     service_create_transaction(app_config.get_ref(), &transaction).await?;
     //HttpResponse::Ok().body(to_string(&transaction))
     Ok(HttpResponse::Ok().body("transaction created!"))
+}
+
+#[get("/transaction/{transaction_id}")]
+pub async fn get_transaction(
+    path: web::Path<String>,
+    app_config: web::Data<AppConfig>,
+) -> Result<HttpResponse, Error> {
+    let transaction_id = path.into_inner();
+    let response_body = service_get_transaction(app_config.get_ref(), transaction_id).await?;
+
+    let response_string = to_string(&response_body)?;
+    Ok(HttpResponse::Ok().body(response_string))
+}
+
+#[get("/transaction")]
+pub async fn get_transactions(app_config: web::Data<AppConfig>) -> Result<HttpResponse, Error> {
+    let transactions = service_get_all_transactions(app_config.get_ref()).await?;
+
+    let response_string = to_string(&transactions)?;
+
+    Ok(HttpResponse::Ok().body(response_string))
 }
